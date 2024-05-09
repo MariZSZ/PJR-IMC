1bf75d5a32bd0ba2b903f413abf4ecd35291ad1c
MariZSZ <zanonimariana033@gmail.com>
2024-05-09 10:36:21 -0300

    Resultado IMC

---
+++ SRC/Components/Form/ResultImc/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ResultImc(props){
+    return(
+        <view>
+            <text>{props.messageResultImc}</text>
+            <text>{props.ResultImc}</text>
+        </view>
+    );
+    }
