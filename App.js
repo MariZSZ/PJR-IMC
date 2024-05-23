8b4d6da4b4db5abc26b6015c87a77d19e4feea54
MariZSZ <zanonimariana033@gmail.com>
2024-05-23 10:47:11 -0300

    Função result

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './SRC/Components/Title';
-import Main from './SRC/Components/Main';
-
+import Form from './SRC/Components/Form';
 export default function App() {
   return (
     <View style={styles.container}>
     <Title></Title>
+    <Form/>
     </View>
   );
 }
--- SRC/Components/Form/index.js
+++ SRC/Components/Form/index.js
@@ -1,11 +1,11 @@
 import React, {useState} from "react";
 import { View, Text, TextInput, Button } from "react-native";
-
+import ResultImc from "./ResultImc";
 export default function Form(){
 
     const [height, setHeight] = useState(null)
     const [weight, setWeight] = useState(null);
-    const [massageImc, setMessageImc] = useState("preencha seu peso e sua altura");
+    const [messageImc, setMessageImc] = useState("preencha seu peso e sua altura");
     const [imc, setImc] = useState(null)
     const [textButton, setTextButton] = useState("Calcular IMC")
 
@@ -20,16 +20,21 @@ export default function Form(){
         <View>
             <Text>Altura</Text>
             <TextInput 
+            onChangetext={setHeight}
+            value = {height}
             placeholder="Ex.1.75"
             keyboardType="numeric"/>
 
             <Text>Peso</Text>
             <TextInput 
+           onChangetext={setWeight}
+           value = {weight}
             placeholder="Ex.85.766"
             keyboardType="numeric"
             />
             
             <Button 
+            onPress={()=> validtionImc()}
             title={textButton}
             
             
@@ -37,7 +42,10 @@ export default function Form(){
 
 
         </View>
-
+        <ResultImc
+        messageResultImc = {messageImc}
+        ResultImc = {imc}
+        />
     </View>
     );
 }
